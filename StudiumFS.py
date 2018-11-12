d382b89dca3898b2dd7bf3b98933fafd1cb043a8
Giuseppe Sottosanti <peppe.sottosanti@gmail.com>
2018-11-12 16:10:42 +0100

    Il FS recupera tutti gli annunci di tutti i Dipartimenti

--- StudiumFS.py
+++ StudiumFS.py
@@ -3,6 +3,7 @@ from __future__ import print_function, absolute_import, division
 import logging
 from getpass import getpass
 
+import requests
 from collections import defaultdict
 from errno import ENOENT
 from stat import S_IFDIR, S_IFLNK, S_IFREG
@@ -24,32 +25,53 @@ auth = {
     'login': raw_input('Nome utente: '),
     'password': getpass('Password: ')
 }
-
-url = 'http://studium.unict.it/dokeos/2018/main/'
-urlAnnouncements = url + 'announcements/'
+url = 'http://studium.unict.it/dokeos/2018/'
 
 class Memory(LoggingMixIn, Operations):
 
-    def __init__(self):     # Crea file e cartelle appena viene montato il VFS
-        self.teachings = FileManager().get_teachings()
-        self.files = {}
-        self.data = defaultdict(bytes)
-        self.fd = 0
-        now = time()
-        self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=3)
-        for teaching in self.teachings:
-            self.files['/' + teaching['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
-            files_array = FileManager().get_files(urlAnnouncements, auth, teaching['cid'])
-            c = 1
-            for f in files_array:
-                title = f['title']
-                if '/' + teaching['name'] + '/' + title in self.files:
-                    title += '(' + str(c) + ')'
-                    c += 1
-                    print('\nHo modificato un nome\n')
-                self.files['/' + teaching['name'] + '/' + title] = dict(st_mode=(S_IFREG | 0o444), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=1, st_size=len(f['text']))
-                self.data['/' + teaching['name'] + '/' + title] = f['text']
-                self.fd += 1
+    def __init__(self): # Crea file e cartelle appena viene montato il VFS... La cartella 24 CFU e' particolare e va trattata a parte
+        with requests.Session() as session:
+            self.departments = FileManager().get_departments(url, auth, session)
+            session.post(url, data=auth)
+            self.files = {}
+            self.data = defaultdict(bytes)
+            self.fd = 0
+            now = time()
+            self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=3)
+            for department in self.departments:
+                self.files['/' + department['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=3)
+                degreeCourses = FileManager().get_degreeCourses(url, auth, department['cid'], session)
+                for degreeCourse in degreeCourses:
+                    self.files['/' + department['name'] + '/' + degreeCourse['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=3)
+                    teachings = FileManager().get_teachings(url, auth, degreeCourse['cid'], session)
+                    for teaching in teachings:
+                        self.files['/' + department['name'] + '/' + degreeCourse['name'] + '/' + teaching['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+                        files_array = FileManager().get_files(url, auth, teaching['cid'], session)
+                        c = 1
+                        for f in files_array:
+                            title = f['title']
+                            if '/' + department['name'] + '/' + degreeCourse['name'] + '/' + teaching['name'] + '/' + title in self.files:
+                                title += '(' + str(c) + ')'
+                                c += 1
+                            self.files['/' + department['name'] + '/' + degreeCourse['name'] + '/' + teaching['name'] + '/' + title] = dict(st_mode=(S_IFREG | 0o444), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=1, st_size=len(f['text']))
+                            self.data['/' + department['name'] + '/' + degreeCourse['name'] + '/' + teaching['name'] + '/' + title] = f['text']
+                            self.fd += 1
+            department = dict(name='CORSI 24 CFU', cid='24CFU')
+            self.departments += [department]
+            self.files['/' + department['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=3)
+            teachings = FileManager().get_teachings(url, auth, department['cid'], session)
+            for teaching in teachings:
+                self.files['/' + department['name'] + '/' + teaching['name']] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+                files_array = FileManager().get_files(url, auth, teaching['cid'], session)
+                c = 1
+                for f in files_array:
+                    title = f['title']
+                    if '/' + department['name'] + '/' + teaching['name'] + '/' + title in self.files:
+                        title += '(' + str(c) + ')'
+                        c += 1
+                    self.files['/' + department['name'] + '/' + teaching['name'] + '/' + title] = dict(st_mode=(S_IFREG | 0o444), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=1, st_size=len(f['text']))
+                    self.data['/' + department['name'] + '/' + teaching['name'] + '/' + title] = f['text']
+                    self.fd += 1
 
     def getattr(self, path, fh=None):
         if path not in self.files:
@@ -71,8 +93,8 @@ class Memory(LoggingMixIn, Operations):
                 if str(path) in str(filePath):
                     list += [fileName]
         else:
-            for teaching in self.teachings:
-                list += [teaching['name']]
+            for department in self.departments:
+                list += [department['name']]
         return list
 
 if __name__ == '__main__':
--- src/scraping.py
+++ src/scraping.py
@@ -1,5 +1,4 @@
 import bs4
-import requests
 import sys
 from HTMLParser import HTMLParser
 
@@ -15,7 +14,8 @@ class MyHTMLParser(HTMLParser):
         self.data += data
 
 class FileManager():
-    def get_file(self, url, session):   # Recupera un singolo annuncio
+
+    def get_file(self, url, session):   # Recupera i dati di un singolo annuncio
         pageText = session.get(url)
         title = bs4.BeautifulSoup(pageText.text, 'html.parser').find(class_='announcement_title').string
         announcementBS4 = bs4.BeautifulSoup(pageText.text, 'html.parser').find(class_='announcement_content').contents
@@ -26,23 +26,46 @@ class FileManager():
         parser.feed(announcement)
         return dict(title=title, text=parser.data)
 
-    def get_files(self, url, auth, cidReq): # Recupera tutti gli annunci di una materia
-        with requests.Session() as session:
-            session.post(url + 'announcements.php', data=auth)
-            pageText = session.get(url + 'announcements.php?cidReq='+cidReq)
-            list = bs4.BeautifulSoup(pageText.text, 'html.parser').find_all('a')
-            lista_files = []
-            for x in list:
-                if(x.get('href') and x.get('title')):
-                    lista_files.append(self.get_file(url + x.get('href'), session))
-            return lista_files
+    def get_files(self, url, auth, cidReq, session): # Recupera tutti gli annunci di una materia
+        localUrl = 'main/announcements/'
+        session.post(url + localUrl + 'announcements.php', data=auth)
+        pageText = session.get(url + localUrl + 'announcements.php?cidReq=' + cidReq)
+        list = bs4.BeautifulSoup(pageText.text, 'html.parser').find_all('a')
+        lista_files = []
+        for x in list:
+            if(x.get('href') and x.get('title')):
+                lista_files.append(self.get_file(url + localUrl + x.get('href'), session))
+        return lista_files
+	
+    def get_departments(self, url, auth, session): # Recupera il nome delle cartelle relative ai dipartimenti
+        session.post(url, data=auth)
+        pageText = session.get(url)
+        list = bs4.BeautifulSoup(pageText.text, 'html.parser').find(class_='home_cats').find('ul').find_all('a')
+        degreeCourses = []
+        for x in list:
+            if x.get('href').split('=')[-1] != '24CFU':
+                degreeCourses.append(dict(name = x.text, cid = x.get('href').split('=')[-1]))
+        return degreeCourses
 
-    def get_teachings(self): # Codice statico temporaneo
-        combo = [{'name': 'Sistemi Operativi', 'cid': '9114'}, {'name': 'Elementi di Analisi Matematica M-Z', 'cid': '9111'}]
-        return combo
+    def get_degreeCourses(self, url, auth, category, session): # Recupera il nome delle cartelle relative ai CdL
+        pageText = session.get(url + 'index.php?category=' + str(category))
+        list = bs4.BeautifulSoup(pageText.text, 'html.parser').find(class_='home_cats').find('ul').find_all('a')
+        degreeCourses = []
+        for x in list:
+            degreeCourses.append(dict(name = x.text, cid = x.get('href').split('=')[-1]))
+        return degreeCourses
+
+    def get_teachings(self, url, auth, category, session): # Recupera nome e codice di una materia
+        session.post(url, data=auth)
+        pageText = session.get(url + 'index.php?category=' + str(category))
+        list = bs4.BeautifulSoup(pageText.text, 'html.parser').find(class_='home_cats').find('ul').find_all('a')
+        teachings = []
+        for x in list:
+            teachings.append(dict(name = x.text, cid = x.get('href').split('/')[-2].split('=')[-1]))
+        return teachings
 
 if __name__ == '__main__':
-    url = 'http://studium.unict.it/dokeos/2018/main/'
+    url = 'http://studium.unict.it/dokeos/2018/'
     auth = {
         'login': raw_input('Nome utente: '),
         'password': getpass('Password: ')
